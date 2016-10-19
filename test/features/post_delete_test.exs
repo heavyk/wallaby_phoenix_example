0469cbdbba60d6a3ec7183aac4fcfb250af38ea6
flames of love <mechanicofthesequence@gmail.com>
2016-10-19 19:52:11 +0200

    fix a few things with the test 1. dangling pipe 2. tr element also contains the th elements (so there will always be more than one) 3. phantomjs does not allow interaction with alert/prompt/confirm - so we hack around it :)

--- test/features/post_delete_test.exs
+++ test/features/post_delete_test.exs
@@ -9,13 +9,17 @@ defmodule WallabyPhoenix.PostDeleteTest do
       |> fill_in("Body", with: "This is a test")
       |> click_on("Submit")
 
+    # dumb hack to confirm the prompt "Are you sure you want to..."
+    # phantomjs does not allow interaction with alert boxes.
+    # however, this will work:
+    session |> execute_script("window.confirm = window.alert = window.prompt = function () { return true }")
+
     page
     |> find(".post")
     |> click_link("Delete")
-    |> 
 
     page |> take_screenshot
 
-    assert page |> find("tr", count: 0)
+    assert page |> find(".post", count: 0)
   end
 end
